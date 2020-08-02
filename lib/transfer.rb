7fa2f126b05a9143201e86f6e77ff94fcbcb01f5
Nasrin Rahimi <77.nasrin@gmail.com>
2020-08-02 10:55:20 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -17,7 +17,7 @@ class Transfer
  end
  
  def execute_transaction
-   if !valid? || @sender.balance < @amount
+   if valid? || @sender.balance < @amount
      @status = "rejected"
      "Transaction rejected. Please check your account balance." 
    elsif @status == "pending"
